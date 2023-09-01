831868f6331f4b416a40b1e3565225c69958d9ed
alexangria1992 <alex040892@gmail.com>
2023-09-01 22:05:28 +1200

    tailwind and components setup

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import {
+  CustomerReviews,
+  Footer,
+  Subscribe,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  SuperQuality,
+} from "./sections";
+
+const App = () => {
+  return (
+    <main className="relative">
+      {/* <Nav/> */}
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="padding bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding padding-x sm:py-32 py-16">
+        <Subscribe />
+      </section>
+      <section className="padding bg-black padding-x padding-t">
+        <Footer />
+      </section>
+    </main>
+  );
+};
+
+export default App;
